1120f74f54e025ade37e61cdc8bfc88e710bdb6c
Bobby Smith <reversetype@gmail.com>
2021-01-06 14:43:03 -0500

    Updates

---
+++ precache-manifest.4ffc517a100b929a12060255ad8f150e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c24ffe26ca3e4325439fc8888fbc25",
+    "url": "/@ux_bob/yv-iwc/index.html"
+  },
+  {
+    "revision": "4d1fa60df5659998c919",
+    "url": "/@ux_bob/yv-iwc/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "b0723c1dbc287ce8f1fb",
+    "url": "/@ux_bob/yv-iwc/static/js/2.f1c79ab6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/@ux_bob/yv-iwc/static/js/2.f1c79ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1fa60df5659998c919",
+    "url": "/@ux_bob/yv-iwc/static/js/main.9b086252.chunk.js"
+  },
+  {
+    "revision": "a252366e437000c59bbde81a9807bcdf",
+    "url": "/@ux_bob/yv-iwc/static/js/main.9b086252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee3959d9470f1abd68f",
+    "url": "/@ux_bob/yv-iwc/static/js/runtime-main.a194085a.js"
+  }
+]);
